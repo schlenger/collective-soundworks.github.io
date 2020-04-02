78caf04da83a936a869702f245c7e4145366c9e9
Benjamin Matuszewski <benjamin.matuszewski@gmail.com>
2020-04-02 10:17:02 +0200

    updated build file to take accoutn of github action

--- publish.sh
+++ publish.sh
@@ -9,31 +9,32 @@ git add -A
 git commit -m 'deploy'
 git push origin sources
 
-# build
-echo "> build"
-npm run build
+## This is hanlded by github action
+# # build
+# echo "> build"
+# npm run build
 
-echo "> copy README"
-cp README.md docs/.vuepress/dist/README.md
+# echo "> copy README"
+# cp README.md build/README.md
 
-echo "> go to dist folder"
-# navigate into the build output directory
-cd build
+# echo "> go to dist folder"
+# # navigate into the build output directory
+# cd build
 
 
-# if you are deploying to a custom domain
-# echo 'www.example.com' > CNAME
+# # if you are deploying to a custom domain
+# # echo 'www.example.com' > CNAME
 
-echo "> push dist folder on master branch"
-git init
-git add -A
-git commit -m 'deploy'
+# echo "> push dist folder on master branch"
+# git init
+# git add -A
+# git commit -m 'deploy'
 
-# if you are deploying to https://<USERNAME>.github.io
-git push -f git@github.com:collective-soundworks/collective-soundworks.github.io.git master
+# # if you are deploying to https://<USERNAME>.github.io
+# git push -f git@github.com:collective-soundworks/collective-soundworks.github.io.git master
 
-# if you are deploying to https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
+# # if you are deploying to https://<USERNAME>.github.io/<REPO>
+# # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
 
-echo "> back to sources"
-cd -
+# echo "> back to sources"
+# cd -
